4043611475aa32c02d021e78a01d71d4c786e9ba
kishorshrestha21 <iamkishorshrestha@gmail.com>
2021-02-26 23:10:43 -0500

    4 files modified

--- src/app/background-color.pipe.ts
+++ src/app/background-color.pipe.ts
@@ -1,12 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Pipe({
-  name: 'backgrounColor'
+  name: 'backgroundColor'
 })
 export class BackgroundColorPipe implements PipeTransform {
+ constructor(private sanitizer:DomSanitizer){}
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: any, cityCode:string ): any {
+    if(cityCode === 'NY'){
+      return this.sanitizer.bypassSecurityTrustHtml('<div style="background-color:red">' + cityCode + '</dvi>')
+    }
+    else{
+ return this.sanitizer.bypassSecurityTrustHtml('<div style = "background-color :green; color :white;">' + cityCode + '</div>')
+    }
+    
   }
 
 }
